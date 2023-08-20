fd6fc5faeec5e6c23da85ee1632aba07254a1bd2
Anna <34191811+404unf@users.noreply.github.com>
2023-08-19 23:25:57 -0700

    Add like button functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-
-
-
-// When page loads, fetch toys from the database 
-addEventListener('DOMContentLoaded', fetchWinners)
+// When page loads, fetch winners from the database 
+window.addEventListener('load', fetchWinners)
 
 function fetchWinners() {
     fetch('http://localhost:3000/winners')
@@ -38,4 +35,73 @@ function makeCard(data) {
 
         collectionDiv.append(newDiv)
     }
+}
+
+// Like Button Functionality
+// When the like button is clicked, increase the number of likes in the database
+setTimeout(() => {
+    executeLikes()
+}, 3000);
+
+function executeLikes() {
+    const likeButtons = document.querySelectorAll('.like-btn')
+    likeButtons.forEach(button => button.addEventListener('click', addLikes))
+}
+
+function addLikes(event) {
+    event.preventDefault()
+    const id = this.id
+
+    // Get current number of likes
+    fetch(`http://localhost:3000/winners/${id}`)
+        .then(response => response.json())
+        .then(data => processLikes(data))
+
+    function processLikes(data) {
+        let numberOfLikes = parseInt(data.likes)
+        numberOfLikes++
+
+        // Update number of likes
+        fetch(`http://localhost:3000/winners/${id}`, {
+            method: 'PATCH',
+            headers:
+            {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                'likes': numberOfLikes
+            })
+        })
+            .then(response => response.json())
+            .then(data => updateLikes(data))
+    }
+
+    function updateLikes(data) {
+        const id = data.id
+        const numbLikes = data.likes
+        const likesP = document.getElementById(id).previousSibling
+        likesP.textContent = `${numbLikes} Likes`
+    }
+
+}
+
+
+
+
+
+// Reveal Stats when Card is Clicked
+// allAvatars = document.querySelectorAll(".winner-avatar")
+// allAvatars.forEach(card => card.addEventListener('click',revealStats));
+
+function retrieveStats(params) {
+    
+}
+
+function populateStats(params) {
+    
+}
+
+function revealStats(params) {
+    
 }
